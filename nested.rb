ff9919b5b5ce99583d8316d18492ed78392ad17c
Matt Milton <mattmilton57@gmail.com>
2020-01-12 22:02:46 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -81,9 +81,10 @@ def adding_matz
           :known_for => "Unix",
           :languages => ["C"]
         }}
-programmer_hash[:yukihiro_matsumoto] = [:known_for['Ruby']]
-programmer_hash[:yukihiro_matsumoto][:languages] = 'LISP'
-programmer_hash[:yukihiro_matsumoto][:languages] = 'C'
+programmer_hash[:yukihiro_matsumoto] = { 
+  known_for: 'Ruby'
+  languages: ['LISP', 'C']
+}
  return programmer_hash
     
 end
